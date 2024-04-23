09c6600c71083199b65fe495eabdf1b91787a740
Andreas Nigg <andreas.nigg@devopsandmore.com>
2024-04-23 12:51:23 +0200

    test

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,26 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+    "/api/user/created",
+    "/api/billing/new-tier",
+    "/api/billing/webhook",
+    "/sign-up/(.*)",
+    "/sign-in/(.*)",
+    "/sign-in",
+    "/sign-out",
+    "/forgot-password",
+])
+
+export default clerkMiddleware((auth, req) => {
+    if (isPublicRoute(req)) return // if it's a public route, do nothing
+    console.log(
+        "Protecting route",
+        req.nextUrl.origin + "" + req.nextUrl.pathname,
+        auth().sessionId
+    )
+    auth().protect() // for any other route, require auth
+})
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
