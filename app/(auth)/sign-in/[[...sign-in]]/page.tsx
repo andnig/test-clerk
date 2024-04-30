99aedc454647021f69e2b7861772b743886abaf4
Andreas Nigg <andreas.nigg@devopsandmore.com>
2024-04-30 19:39:43 +0200

    sjadkl;

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -10,7 +10,7 @@ export default function SignInPage({
 }) {
     return (
         <div className="">
-            <SignIn />
+            <SignIn afterSignInUrl={"/?9"} />
         </div>
     )
 }
