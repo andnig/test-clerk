8c82e80ef366a767b39dd6d6f5186fb4d65e72c2
Andreas Nigg <andreas.nigg@devopsandmore.com>
2024-05-09 07:51:35 +0200

    upgrade middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,25 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+// import { authMiddleware } from "@clerk/nextjs/server";
+//
+// export default authMiddleware({});
+//
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+//
 
-export default authMiddleware({});
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)"]);
+
+export default clerkMiddleware(
+  (auth, req) => {
+    if (isPublicRoute(req)) return;
+    auth().protect();
+  },
+  {
+    debug: true,
+  },
+);
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
